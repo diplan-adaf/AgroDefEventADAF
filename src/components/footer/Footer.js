fcb539d1a8889deba01d5557b375d48e4afb1896
MatheusVictor23 <matheuszaya@gmail.com>
2025-08-06 11:14:21 -0400

    refactor: alterações footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,19 +1,23 @@
 import { Mail, Phone } from "lucide-react";
 import "./footer.css"; // Assuming you have a CSS file for styling
-
+import logo from "../../assets/descricaoBanner.png"; // Adjust the path as necessary
+import triade from "../../assets/triade.png"; // Adjust the path as necessary
 export default function Footer(){
     return(
     <footer className="site-footer">
-        <h3 className="footer-title">Fale conosco</h3>
+      <div className="site-logo">
+        <img src={logo} alt="Logo" className="footer-logo" />
+      </div>
+      <div className="footer-info">
+        <h2>Fale conosco</h2>
         <div className="footer-contact">
-          <p>
-            <Phone className="icon" /> (92) 8113-6607 | (92) 9330-6826 | (92)
-            9160-7182 | (92) 9195-2657
-          </p>
-          <p>
-            <Mail className="icon" /> agrodefesaevento@gmail.com
-          </p>
+          <span>Tel <Phone />: (92) 99999-9999 | (92) 99999-9999 | (92) 99999-9999 </span>
+          <span>Email <Mail />: contato@gmail.com.br</span>
         </div>
+      </div>
+      <div className="footer-triade">
+        <img src={triade} alt="Triade" className="footer-triade" />
+      </div>
     </footer>
     )
 }
