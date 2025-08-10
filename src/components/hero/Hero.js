8f2694a635f9dd19effa73becbfbd029dc4b72f3
MatheusVictor23 <matheuszaya@gmail.com>
2025-08-10 14:01:24 -0400

    feat: conversão imagem hero para webp

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
     <>
       <div className="hero-img">
         <img src={descricaoBanner} alt="Hero" />
-        <span>2 a 3 de Setembro - Vasco Vasques | Etapa 1</span>
+        <span>2 e 3 de Setembro - Vasco Vasques | Etapa 1</span>
         <Button text={"Acompanhe-nos"} ref={"https://www.instagram.com/agro_defesa_evento/"} cls={"instagram-hero"}>
           <Instagram className="icon" />
         </Button>
