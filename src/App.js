bf6edad168ccad92527351aacdfa0db8fe8f9433
lucas <lucas.oklp@gmail.com>
2025-08-18 16:27:37 -0300

    apagando redirecionamento

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AgroAmazonasSite from "./pages/AgroAmazonasSite.js";
 import ScrollTop from "./utils/ScrollTop.js";
-import { useEffect } from "react";
 import { Suspense, lazy } from "react";
 const PalestrantesPage = lazy(() => import("./pages/PalestrantesPage"));
 
 function App() {
-  useEffect(() => {
-    window.location.replace("https://www.agrodefesaam.com.br");
-  }, []);
-
   return (
     <Router>
       <Suspense fallback={<div>Carregando...</div>}>
